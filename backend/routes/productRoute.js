a1b3b1b5ceb48ac2eb75547cdf41ec834e48a697
Priyanka <priyankaqaz12345@gmail.com>
2022-03-24 16:29:44 +0530

    Backend User Routes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,36 @@
-const express=require('express');
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require('../controllers/productController');
+// 04:08:27 Making Order API
+
+const express = require("express");
+const {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductDetails,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
+} = require("../controllers/productController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 // cretaed mini-app
-const router=new express.Router();
+const router = new express.Router();
 
 router.route("/products").get(getAllProducts);
-router.route("/product/new").post(isAuthenticatedUser, authorizeRoles("admin"),createProduct);;
-router.route("/product/:id").get(getProductDetails).put(isAuthenticatedUser, authorizeRoles("admin"),updateProduct).delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct);
+router
+  .route("/admin/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router
+  .route("/admin/product/:id")
+  .get(getProductDetails)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
-module.exports=router;
+router.route("/product/:id").get(getProductDetails);
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
+
+module.exports = router;
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,44 @@
-const express=require('express');
-const { registerUser,loginUser, logout, forgotPassword,resetPassword } = require('../controllers/userController');
-const router=new express.Router();
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  logout,
+  forgotPassword,
+  resetPassword,
+  getUserDetails,
+  updateProfile,
+  getAllUser,
+  getSingleUser,
+  updateUserRole,
+  deleteUser,
+} = require("../controllers/userController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
+const router = new express.Router();
 
 router.route("/register").post(registerUser);
+
 router.route("/login").post(loginUser);
+
 router.route("/paswword/forgot").post(forgotPassword);
+
 router.route("/paswword/reset/:token").put(resetPassword);
+
 router.route("/logout").get(logout);
 
-module.exports=router;
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUser);
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
+
+module.exports = router;
