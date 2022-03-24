dce46dee4a5e93802b6a4b95835308dda21f1e9f
Priyanka <priyankaqaz12345@gmail.com>
2022-03-25 03:09:10 +0530

    25-03-202

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,8 @@
 // Configuration file imported
 require("dotenv").config({ path: "backend/.env" });
-
-// Imported Express app
 const app = require("./app");
-
-// Database imported
 const database = require("./mongoose_connection/database");
+const cloudinary = require("cloudinary");
 
 // Handling Uncaught Exceptions-should be wriiten at very first.
 process.on("uncaughtException", (err) => {
@@ -14,8 +11,19 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
+
 // Database connected.
 database();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`We are listening to the port `);
@@ -25,6 +33,7 @@ const server = app.listen(process.env.PORT || 3000, () => {
 process.on("unhandledRejection", (err) => {
   console.log(`Error:${err.message}`);
   console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
   server.close(() => {
     process.exit(1);
   });
